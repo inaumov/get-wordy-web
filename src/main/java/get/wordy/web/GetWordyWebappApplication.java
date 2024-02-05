23ce7cf73b19655bc999b4f45302dbfc4df2aa71
Igor Naumov <naumov@live.com>
2024-02-05 23:58:34 +0200

    Use spring boot 3.x as a server now

---
+++ src/main/java/get/wordy/web/GetWordyWebappApplication.java
@@ -0,0 +1,13 @@
+package get.wordy.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetWordyWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetWordyWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/get/wordy/web/GetWordyWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package get.wordy.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetWordyWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
