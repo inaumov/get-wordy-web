5534d6c0fcd2c493ffc386e62e16b31950b85044
Igor Naumov <naumov@live.com>
2024-03-05 15:50:26 +0200

    Tiny restyle of 404 page

--- static/views/error/NotFoundView.vue
+++ static/views/error/NotFoundView.vue
@@ -10,16 +10,22 @@ export default {
 </script>
 
 <template>
-  <div class="pt-5 align-items-center justify-content-center">
+  <div id="error404" class="pt-5 align-items-center justify-content-center">
     <div class="text-center">
       <h1 class="display-1 fw-bold">404</h1>
-      <p class="fs-4"><span class="text-danger">Oops!</span> Page not found.</p>
+      <p class="fs-4"><span>Oops!</span> Page not found.</p>
       <p class="lead">
         The page you’re looking for does not exist
       </p>
-      <router-link to="/" class="mt-4 btn btn-default btn-lg">
+      <router-link to="/" class="mt-4 btn btn-lg">
         <i class="bi bi-house-door"></i> Take me home
       </router-link>
     </div>
   </div>
 </template>
+
+<style>
+div#error404 i, div#error404 span {
+  color: rgb(185, 87, 84)
+}
+</style>
