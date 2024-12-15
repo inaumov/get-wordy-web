7d8efda430e910f7b40ec779c58682021f81a983
Igor Naumov <naumov@live.com>
2024-12-16 00:43:44 +0200

    Show attendees on classes info list view

--- static/views/ClassListView.vue
+++ static/views/ClassListView.vue
@@ -23,7 +23,7 @@ export default {
         // add the 'hasAttendees' property based on the condition
         return {
           ...item, // spread the existing properties
-          hasAttendees: item['attendees'] && item['attendees'] >= 1
+          hasAttendees: item['attendees'] && item['attendees'].length >= 1
         };
       });
     },
@@ -40,6 +40,29 @@ export default {
       this.router.push({
         name: 'add-new-class'
       });
+    },
+    // method to shuffle colors randomly
+    shuffleColors() {
+      const pastelColors = [
+        'pastel-blue', 'pastel-pink', 'pastel-yellow', 'pastel-green',
+        'pastel-lavender', 'pastel-peach', 'pastel-mint', 'pastel-coral',
+        'pastel-lilac', 'pastel-lemon', 'pastel-sky', 'pastel-beige'
+      ];
+      // shuffle the pastel colors array using the Fisher-Yates algorithm
+      for (let i = pastelColors.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [pastelColors[i], pastelColors[j]] = [pastelColors[j], pastelColors[i]];
+      }
+      return pastelColors;
+    },
+    // get the badge class for a specific attendee in a specific class
+    getBadgeClass(classItem, index) {
+      // if colorOrder is not defined, shuffle colors for this class
+      if (!classItem.colorOrder) {
+        classItem.colorOrder = this.shuffleColors();
+      }
+      // return the class based on the shuffled color order for the class
+      return classItem.colorOrder[index % classItem.colorOrder.length];
     }
   },
   computed: {
@@ -69,13 +92,25 @@ export default {
                 <span>{{ classItem['name'] }}</span>
               </div>
               <div class="class-details">
-                <span><strong>Attendees</strong> : {{ classItem.hasAttendees ? classItem['attendees'] : 'none' }}</span>
+                <span><strong>Attendees</strong>:</span>
+                <div v-if="classItem.hasAttendees">
+                  <span
+                      v-for="(attendee, attendeeIndex) in classItem['attendees']"
+                      :key="attendeeIndex"
+                      :class="getBadgeClass(classItem, attendeeIndex)"
+                      class="badge">
+                    {{ attendee }}
+                  </span>
+                </div>
+                <div v-else>
+                  None
+                </div>
               </div>
-              <div v-if="classItem['classFormat']" class="class-details">
-                <span><strong>Format</strong> : {{ classItem['classFormat'] }}</span>
+              <div v-if="classItem['format']" class="class-details">
+                <span><strong>Format</strong> : {{ classItem['format'] }}</span>
               </div>
-              <div v-if="classItem['classLevel']" class="class-details">
-                <span><strong>Level</strong> : {{ classItem['classLevel'] }}</span>
+              <div v-if="classItem['level']" class="class-details">
+                <span><strong>Level</strong> : {{ classItem['level'] }}</span>
               </div>
               <div v-if="classItem['material']" class="class-details">
                 <span><strong>Materials</strong> : {{ classItem['material'] }}</span>
@@ -122,4 +157,25 @@ export default {
 .day-groups h3 {
   font-size: 1.5rem;
 }
+
+.badge {
+  padding: 0.5rem;
+  margin: 0.25rem;
+  color: DarkSlateGrey;
+  font-weight: bold;
+}
+
+.pastel-blue { background-color: rgba(124, 185, 232, 0.9); }
+.pastel-pink { background-color: rgba(247, 168, 184, 0.9); }
+.pastel-yellow { background-color: rgba(255, 178, 48, 1); }  /* darker yellow */
+.pastel-green { background-color: rgba(168, 213, 186, 0.9); }
+.pastel-lavender { background-color: rgba(185, 168, 217, 0.9); }
+.pastel-peach { background-color: rgba(255, 124, 85, 1); }   /* darker peach/orange */
+.pastel-mint { background-color: rgba(141, 228, 161, 0.9); }
+.pastel-coral { background-color: rgba(255, 111, 97, 0.9); }
+.pastel-lilac { background-color: rgba(216, 167, 208, 0.9); }
+.pastel-lemon { background-color: rgba(255, 228, 87, 1); }  /* slightly darker yellow */
+.pastel-sky { background-color: rgba(163, 201, 255, 0.9); }
+.pastel-beige { background-color: rgba(244, 209, 176, 0.9); }
+
 </style>
