00042083599f92fd7222ce8b9f00fcde5e162bb5
Igor Naumov <naumov@live.com>
2024-12-16 23:37:53 +0200

    Draft: AI integration contract

---
+++ src/main/java/get/wordy/ai/AiService.java
@@ -0,0 +1,13 @@
+package get.wordy.ai;
+
+import get.wordy.ai.model.GetExplanationResult;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AiService {
+
+    public GetExplanationResult requestWordSearch(String searchRequest) {
+        return null;
+    }
+
+}
---
+++ src/main/java/get/wordy/ai/model/GetExplanationResult.java
@@ -0,0 +1,33 @@
+package get.wordy.ai.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GetExplanationResult {
+
+    private String value;
+    private String transcription;
+    private List<Explanation> explanations;
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Explanation {
+        @JsonProperty("part_of_speech")
+        private String partOfSpeech;
+        private String meaning;
+        private List<String> sentences;
+        private List<String> collocations;
+    }
+
+}
