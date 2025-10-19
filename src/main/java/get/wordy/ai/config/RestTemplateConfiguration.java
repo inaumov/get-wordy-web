2c11783acfe04a67818462f4e755d54ce4904755
Igor Naumov <naumov@live.com>
2025-10-19 16:42:17 +0300

    Mocked integration tests for AI Service

--- src/main/java/get/wordy/ai/config/RestTemplateConfiguration.java
+++ src/main/java/get/wordy/ai/config/RestTemplateConfiguration.java
@@ -3,14 +3,9 @@ package get.wordy.ai.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.web.client.RestTemplate;
-
-import java.time.Duration;
 
 @Configuration
 @Slf4j
@@ -21,16 +16,8 @@ public class RestTemplateConfiguration {
 
     @Bean
     @Qualifier("openaiRestTemplate")
-    public RestTemplate openaiRestTemplate(RestTemplateBuilder restTemplateBuilder) {
-
-        restTemplateBuilder
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .setConnectTimeout(Duration.ofSeconds(5))
-                .setReadTimeout(Duration.ofSeconds(30));
-
-        RestTemplate restTemplate = restTemplateBuilder
-                .build();
-
+    public RestTemplate openaiRestTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
         // add auth header like this, otherwise server could respond with error:
         // java.net.HttpRetryException: cannot retry due to server authentication, in streaming mode
         restTemplate.getInterceptors().add((request, body, execution) -> {
